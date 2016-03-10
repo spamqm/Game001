d5f63a6793580b154de35a5759cafefeec240bd4
Greg Kover <@spamqm.mail.ru>
2016-03-10 23:35:04 +0300

    first commit

---
+++ src/SomeActions.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Gregory on 08.03.2016.
+ */
+public class SomeActions {
+
+    public SomeActions() {
+        log("Hello world");
+
+
+    }
+
+    public void log(String s) {
+        System.out.println(s);
+    }
+
+}
+
+
---
+++ src/Starter.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Gregory on 08.03.2016.
+ */
+public class Starter {
+    public static void main(String[] args) {
+        new SomeActions();
+
+    }
+}
