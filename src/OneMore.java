58661276c550785df50eca26c6cf737ebf7db3fb
Greg Kover <@spamqm.mail.ru>
2016-03-11 13:26:47 +0300

    PlugIn

--- src/OneMore.java
+++ src/OneMore.java
@@ -2,4 +2,9 @@
  * Created by Gregory on 11.03.2016.
  */
 public class OneMore {
+
+
+    public void info() {
+        System.err.print("Some data with error");
+    }
 }
--- src/Starter.java
+++ src/Starter.java
@@ -2,6 +2,9 @@ public class Starter {
     public static void main(String[] args) {
         new SomeActions("Hi").getDate();
 
+        new OneMore();
+
+        new OneMore().info();
 
     }
 }
